25611a7270c3d35f1ffe6a5d083f005469d11fbb
Alexander <alexander.khrapko@gmail.com>
2026-01-27 13:26:55 +0200

    new file

---
+++ Mudule_4_task_1.py
@@ -0,0 +1,36 @@
+import os
+# файл содержит информацию о сотрудниках и их зараплате
+# данные разделены без пробела запятой
+
+# Алекс Корп,3000
+# Никита Борисенко,2000
+# Sitarama Raju,1000
+# Никита Борисенко.2000 ошибка
+
+def total_salary(path) -> tuple[int, float]:
+    # финция делает просчет общей зараплты всех сотрудников и среднюю зп.
+    total = 0
+    count = 0
+
+    with open(path, 'r', encoding='utf-8') as fh:
+        for line in fh:
+            line = line.strip()
+            if not line or ',' not in line:
+                # проверяет конец файла и отсутствие "запятой", чтобы не было
+                # ошибки по разделителю "точка" и не увеличивать coutn
+                continue
+            try:
+                salary = int (line.split(',', 1)[1])
+                # разбивает строку по "запятой", один раз (по первому вхождению)
+                # [1] берет второй элемент списка после разделения строки.
+                # [0] это первый элемент списка до "запятой"
+                total += salary
+                count += 1
+            except ValueError:
+                continue
+        return total, total/count
+
+os.system('cls')
+total, average = total_salary ('salary.txt')
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
