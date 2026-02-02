d43fe58355864971174fde985235abee856a4dc8
Alexander <alexander.khrapko@gmail.com>
2026-02-02 09:07:59 +0200

    add comments for task 4

--- Module_4_task_4.py
+++ Module_4_task_4.py
@@ -10,6 +10,8 @@ def add_contact(args:list,contacts:dict)->str:
     name = args[0]
     phone = args[1]
 
+    # проверка на существования контакта, но даже если он существует,
+    # то телефон все равно меняется (условие задачи).
     if name in contacts:
         contacts[name] = phone
         return f'The contact: {name} already exists. Contact added.'
@@ -21,6 +23,8 @@ def change_contact(args:list,contacts:dict)->str:
     name = args[0]
     phone = args[1]
 
+    # проверка на существования контакта по имени, если да, то происходит замена
+    # если нет выводится сообщение об отсутствии контакта.
     if name in contacts:
         contacts[name] = phone
         return f"Contact {name} changed"
@@ -30,25 +34,33 @@ def change_contact(args:list,contacts:dict)->str:
 def show_phone(args:list,contacts:dict)->str:
     name = args[0]
 
+    # проверка на существования контакта по имени, если да, то выводится полная информация
+    # о контакте, если нет выводится сообщение об отсутствии контакта.
     if name in contacts:
         return f"Contact {name} - {contacts[name]}"
     else:
         return f"Contact {name} not found"
 
+# для более наглядной информации о списке контактов и телефонов - создаем список из строк
+# разделенного переносом строки '\n'
 def show_all(contacts:dict)->str:
     if len(contacts) == 0:
         return 'No contacts in the list.'
 
-    lines = []  # создаём пустой список для строк контактов
+    lines = []                              # создаём пустой список для строк контактов
 
-    for name, phone in contacts.items():  # проходим по каждому элементу словаря
+    for name, phone in contacts.items():    # проходим по каждому элементу словаря
         contact_str = name + " — " + phone  # формируем строку для одного контакта
-        lines.append(contact_str)  # добавляем эту строку в список
+        lines.append(contact_str)           # добавляем эту строку в список
 
-    result = "\n".join(lines)  # объединяем все строки через перенос строки
+    result = "\n".join(lines)               # объединяем все строки через перенос строки
     return result
 
-
+# основная функция в которой происходит ввод командной строки с аргументами
+# команды add, change проверяется количество передаваемый аргументов, если не два
+# то команда считается некорректной
+# для команды phone проверяется на один передаваемый аргумент, если он не один
+# команда считается некорректной
 def main():
     contacts = {}
     os.system('cls')
